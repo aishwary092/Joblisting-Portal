f46365337ef46d4496ee9a978961a68d6be72dbf
Aishwary <aishwary092@example.com>
2026-04-30 09:06:03 +0530

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 // Function to connect to the MongoDB database
 
 const connectDB = async () => {
+    const uri = 'mongodb://localhost:27017/job-portal'; // Using local MongoDB
+    console.log('Connecting to MongoDB:', uri);
 
     mongoose.connection.on('connected',()=> console.log('Database Connected'))
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
+    await mongoose.connect(uri)
     console.log('MongoDB connection attempt made')
 }
 
